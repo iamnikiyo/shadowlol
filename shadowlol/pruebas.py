d52e4ed77f73dce6fe9a72bdfe626bf4b6a70fbc
bynikiyo <nikiyo9@gmail.com>
2016-04-21 10:47:15 +0100

    added new requirement to requirements file

--- shadowlol/pruebas.py
+++ shadowlol/pruebas.py
@@ -1,10 +1,11 @@
 from cassiopeia import riotapi
 
 riotapi.set_region("EUW")
-riotapi.set_api_key("64a3b34b-e65d-4867-adb6-47e33cf2dfc3")
-summoners = riotapi.get_challenger()
-
-for i in range(10):
-	print (summoners[i].summoner.name.encode('utf-8'))
-
-
+riotapi.set_api_key("deef6b4f-d2b2-49a1-8aaf-9128a2fc54e3")
+summoner = riotapi.get_summoner_by_name("bynikiyo")
+recents = riotapi.get_recent_games(summoner)
+for i in range(5):
+	print("------Partido {}----------".format(i))
+	for participant in recents[i]:
+		print(participant.summoner.name + " y jugó con : " + participant.champion.name)
+	print("------FIN Partido {}----------".format(i))		
