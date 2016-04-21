1ba4282a3d88b1edf9838aba7b9e5b03d56765de
bynikiyo <nikiyo9@gmail.com>
2016-04-21 13:12:42 +0100

    changes whitenoise

--- shadowlol/settings.py
+++ shadowlol/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
